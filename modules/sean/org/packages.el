e9f1378c5ad3470929cc871d4574084389678a3d
nnysldrwv <yuanxiang424@gmail.com>
2024-06-04 23:13:17 +0800

    enable evil

--- modules/sean/org/packages.el
+++
@@ -1,8 +0,0 @@
-;; -*- no-byte-compile: t; -*-
-;;; cnsunyour/org/packages.el
-
-
-(package! counsel-org-clock)
-(package! grip-mode)
-(package! ox-gfm)
-(package! org-modern)
--- modules/sean/translate/packages.el
+++
@@ -1,7 +0,0 @@
-;; -*- no-byte-compile: t; -*-
-;;; cnsunyour/translate/packages.el
-
-(package! names)
-(package! youdao-dictionary)
-(package! fanyi)
-(package! go-translate)
