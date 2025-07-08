38dee7df7f0deb5ee9ab8489c5de2299f1928031
nnysldrwv <31592021+nnysldrwv@users.noreply.github.com>
2025-07-08 09:48:06 -0700

    换主题；解决yaml tree-sitter报错问题

--- custom.el
+++ custom.el
@@ -4,6 +4,11 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
+ '(custom-safe-themes
+   '("1bc640af8b000ae0275dbffefa2eb22ec91f6de53aca87221c125dc710057511"
+     "d97ac0baa0b67be4f7523795621ea5096939a47e8b46378f79e78846e0e4ad3d"
+     "4d714a034e7747598869bef1104e96336a71c3d141fa58618e4606a27507db4c"
+     "19d62171e83f2d4d6f7c31fc0a6f437e8cec4543234f0548bad5d49be8e344cd" default))
  '(ignored-local-variable-values '((eval progn (pp-buffer) (indent-buffer)))))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
